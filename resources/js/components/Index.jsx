9480144b94fa2729f922a3c171346df0e431b31d
isuru <isuru2003a@gmail.com>
2024-02-03 21:07:49 +1100

    :/

--- resources/js/components/Index.jsx
+++ resources/js/components/Index.jsx
@@ -15,8 +15,17 @@ import Router from "./Router.jsx";
 import NotificationPortal from "./NotificationPortal.jsx";
 import UserPopup from "./User/UserPopup.jsx";
 import api from "../scripts/api.js";
+import Loader from "./Loader.jsx";
 
 function Index() {
+
+    /**
+     * Should the application be rendered if API initialisation fails?
+     * This should be set to false if the application relies on data from
+     * the API to be displayed unconditionally on load
+     * @type {boolean}
+     */
+    const renderWithoutApiSuccess = true
 
     /**
      * Status of the initial API call
@@ -114,6 +123,9 @@ function Index() {
                             /**
                              * Place the below line out of the if() statement if you wish for the
                              * application to always redirect to auth server if a user is not found
+                             *
+                             * It is also suggested that renderWithoutApiSuccess is set to false
+                             * to prevent components from trying to render without data
                              */
                             window.location = e.response.data.data.auth_url
                         }
@@ -128,37 +140,43 @@ function Index() {
         <div
             className={`app ${(settings && settings.theme) ? settings.theme : `${settingsCookie ? JSON.parse(settingsCookie).theme : 'system'}`}`}
         >
-            <Logo/>
-            <BrowserRouter>
-                <div className="container">
-                    {/*
-                    Comment out the below line if you do not wish for the user popup to be displayed
-                    when a user is logged on.
-                    It is expected that the popup is not removed if there is no clear indicator of the current
-                    user on the landing page of the application
-                    */}
-                    {user ? <UserPopup/> : ''}
+            {(apiStatus.success || renderWithoutApiSuccess) ?
+                <>
+                    <Logo/>
+                    <BrowserRouter>
+                        <div className="container">
+                            {/*
+                            Comment out the below line if you do not wish for the user popup to be displayed
+                            when a user is logged on.
+                            It is expected that the popup is not removed if there is no clear indicator of the current
+                            user on the landing page of the application
+                            */}
+                            {user ? <UserPopup/> : ''}
 
-                    {/*
-                    In the scenario that the user popup is removed, the below component should be
-                    replaced with <UserIconStatic/> to account for the animation delay(s)
-                    */}
-                    <UserIcon apiStatus={apiStatus}/>
+                            {/*
+                            In the scenario that the user popup is removed, the below component should be
+                            replaced with <UserIconStatic/> to account for the animation delay(s)
+                            */}
+                            <UserIcon apiStatus={apiStatus}/>
 
-                    {/*
-                    Only components that should be rendered application wide should be placed here
-                    All other components should be placed inside the router following proper
-                    routing structure
-                    */}
-                    <div className="content-wrapper">
-                        <ModalPortal/>
-                        <NotificationPortal/>
-                        <Router/>
-                    </div>
-                </div>
-            </BrowserRouter>
-            {/*This signature MUST be present on all production Nonverse applications*/}
-            <span id="signature">Micky & Rex Co<span className="splash">.</span></span>
+                            {/*
+                            Only components that should be rendered application wide should be placed here
+                            All other components should be placed inside the router following proper
+                            routing structure
+                            */}
+
+                            <div className="content-wrapper">
+                                <ModalPortal/>
+                                <NotificationPortal/>
+                                <Router/>
+                            </div>
+                        </div>
+                    </BrowserRouter>
+                    {/*This signature MUST be present on all production Nonverse applications*/}
+                    <span id="signature">Micky & Rex Co<span className="splash">.</span></span>
+                </>
+                : <Loader/>
+            }
         </div>
     );
 }
