7e77ae2af372039aff48a21a0a7e566092dd315b
isuru <isuru2003a@gmail.com>
2024-02-03 15:58:26 +1100

    Animate user popup

--- resources/js/components/User/UserPopup.jsx
+++ resources/js/components/User/UserPopup.jsx
@@ -1,18 +1,47 @@
 import {useSelector} from "react-redux";
+import {motion} from "framer-motion";
+import {useEffect, useState} from "react";
 
 const UserPopup = () => {
 
     const user = useSelector(state => state.user.value)
+    const [animation, setAnimation] = useState({
+        width: '50px',
+        marginTop: 0,
+        opacity: 1
+    })
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAnimation({
+                ...animation,
+                width: 'auto'
+            })
+        }, 300)
+        setTimeout(() => {
+            setAnimation({
+                ...animation,
+                width: '50px',
+                opacity: 0
+                // TODO Need to have popup actaully dismount instead of going invisible
+            })
+        }, 3000)
+    }, [])
 
     return (
-        <div className="user-popup">
+        <motion.div
+            className="user-popup"
+            key={`user-popup-${user.uuid}`}
+            initial={{width: '50px', opacity: 0, marginTop: '-100px'}}
+            animate={animation}
+        >
             <div id="user-popup-icon">
                 <h1>{user.name_first.charAt(0).toUpperCase()}</h1>
             </div>
             <div id="user-popup-text">
                 <span id="user-popup-name">{`${user.name_first} ${user.name_last}`}</span>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
